7820184f6f21a2ed79484c13fa8d572a4ff5a361
K-da-fire <105974458+K-da-fire@users.noreply.github.com>
2024-12-23 11:14:42 +0900

    first commit

---
+++ src/test/java/com/example/trelloteamproject/TrelloTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trelloteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
