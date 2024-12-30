9bd0386e88c86497c1973e04ee6e26ef2be587a5
Jun Heo <huhjune98@naver.com>
2024-12-30 07:51:23 -0500

    add: slack 알림 코드 추가

---
+++ src/main/java/com/example/trelloteamproject/notification/controller/NotificationController.java
@@ -0,0 +1,18 @@
+package com.example.trelloteamproject.notification.controller;
+
+import com.example.trelloteamproject.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @PostMapping("/notifications")
+    public void notifySlack(@RequestBody String message) {
+        notificationService.sendMessageToSlack(message);
+    }
+}
---
+++ src/main/java/com/example/trelloteamproject/notification/service/NotificationService.java
@@ -0,0 +1,5 @@
+package com.example.trelloteamproject.notification.service;
+
+public interface NotificationService {
+    void sendMessageToSlack(String message);
+}
